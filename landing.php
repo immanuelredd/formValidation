49489fa7727ab8105e4385d59d44d9ca8b36146e
otjake <jaketuriacada@gmail.com>
2020-08-06 18:08:36 -0700

    landing page on successful registration

---
+++ landing.php
@@ -0,0 +1,44 @@
+
+<?php
+if(!empty($_GET['color'])){
+     $color=$_GET['color'];
+}else{
+    echo 'ungetted';
+}
+
+?>
+<html lang="en">
+
+<meta http-equiv="content-type" content="text/html;charset=UTF-8" />
+<head>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- CSS only -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.1/css/bootstrap.min.css" integrity="sha384-VCmXjywReHh4PwowAiWNagnWcLhlEJLA5buUprzK8rxFgeH0kww/aWY76TfkUoSX" crossorigin="anonymous">
+<!--    <link href="css/main.css" rel="stylesheet" media="all">-->
+</head>
+<body style="background-color:<?php echo $color; ?>">
+
+
+<div class="container">
+    <div class="row">
+        <div class="col-12">
+            <div class="card mb-3 ">
+                <img src="image/welcome.jpg" class="card-img-top" alt="...">
+        <div class="alert alert-success" style="text-align: center"><h3>The amount of knowledge you have is how small your limit is.</h3></div>
+            </div>
+        </div>
+    </div>
+
+
+
+</div>
+<!-- JS, Popper.js, and jQuery -->
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.1/js/bootstrap.min.js" integrity="sha384-XEerZL0cuoUbHE4nZReLT7nx9gQrQreJekYhJD9WNWhH8nEW+0c5qq7aIo2Wl30J" crossorigin="anonymous"></script>
+</body>
+
+</html>
+
